b5de7475c123bb419d9214fdf857cd1e9ca61110
David Pérez Martínez <38837156+puruwin@users.noreply.github.com>
2018-05-15 21:22:36 +0200

    Add files via upload

---
+++ Usuario y Contraseña.py
@@ -0,0 +1,11 @@
+username = input("Enter your username ")
+if username == "Puruwin":
+    print ("Hello, Puruwin!")
+    if username == "Puruwin":
+        password = input("Enter your password ")
+        if password == "Python":
+            print("Access permited")
+        else:
+            print("Access denied")
+else:
+    print("Who are you?")
